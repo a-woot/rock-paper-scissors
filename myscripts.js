b83ad0425399efbd40223d5146ddd38c3dbd63fa
Andy Wootton <andywootton@gmx.com>
2022-06-22 10:37:50 +0100

    started creating playRound function

--- myscripts.js
+++ myscripts.js
@@ -5,3 +5,12 @@ function computerPlay(items) {
     return items[Math.floor(Math.random()*items.length)];
     console.log(computerPlay(items));
 }
+
+function playRound(playerSelection, computerSelection) {
+
+
+    
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection))
