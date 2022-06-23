30dad94c7ce3c41712e511a345c5e89ccb50c19e
Andy Wootton <andywootton@gmx.com>
2022-06-23 09:30:19 +0100

    tested IF statements and ammended slightly, now to make playRound function

--- myscripts.js
+++ myscripts.js
@@ -17,26 +17,25 @@ function playRound(playerSelection, computerSelection) {
    }
 
    if (playerSelection == "rock" && computerSelection === "scissors") {
-    return "You win!";
+    return "You win! rock beats scissors!";
    }
    if (playerSelection == "paper" && computerSelection === "scissors") {
-    return "You lose!";
+    return "You lose! paper loses to scissors";
    }
    if (playerSelection == "scissors" && computerSelection === "rock") {
-    return "You lose!";
+    return "You lose! scissors loses to rock";
    }
    if (playerSelection == "paper" && computerSelection === "rock") {
-    return "You win!";
+    return "You win! paper beats rock";
    }
    if (playerSelection == "scissors" && computerSelection === "paper") {
-    return "You win!";
+    return "You win! scissors beats paper";
    }
    if (playerSelection == "rock" && computerSelection === "paper") {
-    return "You lose!";
+    return "You lose! rock loses to paper";
    } 
-
     
 }
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
+console.log(playRound(playerSelection, computerSelection));
