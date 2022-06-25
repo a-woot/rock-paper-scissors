f1a7e2198096568d45169dd5ceb876d10632f3e6
Andy Wootton <andywootton@gmx.com>
2022-06-25 18:35:06 +0100

    changed game loop to test scores working correctly, moved playRound into loop as otherwise game plays 6 times

--- myscripts.js
+++ myscripts.js
@@ -2,11 +2,12 @@ function computerPlay(items) {
     var items = ['rock', 'paper', 'scissors'];
     /*var randomItem =  myArray[Math.floor(Math.random()*myArray.length)];
     return randomItem; */
-    return items[Math.floor(Math.random()*items.length)];
+    return items[Math.floor(Math.random() * items.length)];
     console.log(computerPlay(items));
 }
 let playerScore = 0;
 let compScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     /*let playerSelection = (prompt("Please select rock, paper or scissors "));
     if(playerSelection == "rock") && (computerSelection == "paper"){ 
@@ -15,35 +16,32 @@ function playRound(playerSelection, computerSelection) {
     */
 
 
-   if (playerSelection == computerSelection) {
-    return "Tie, no one wins"
-   }
-   else if ((playerSelection == "paper" && computerSelection === "scissors")|| 
-   (playerSelection == "scissors" && computerSelection === "rock")
-   || (playerSelection == "rock" && computerSelection === "paper")) {
-    return "You lose"}
-   
-
-   else if ((playerSelection == "rock" && computerSelection === "scissors") || (playerSelection == "paper" && computerSelection === "rock")
-   || (playerSelection == "scissors" && computerSelection === "paper"))  {
-    return "You win!";
-    playerScore = ++playerScore;
-    return playerScore;
-   }
-
-   
- 
+    if (playerSelection == computerSelection) {
+        return "Tie, no one wins"
+    } else if ((playerSelection == "paper" && computerSelection === "scissors") ||
+        (playerSelection == "scissors" && computerSelection === "rock") ||
+        (playerSelection == "rock" && computerSelection === "paper")) {
+        compScore = compScore + 1;
+        return "You lose"
+    } else if ((playerSelection == "rock" && computerSelection === "scissors") || (playerSelection == "paper" && computerSelection === "rock") ||
+        (playerSelection == "scissors" && computerSelection === "paper")) {
+        playerScore = playerScore + 1;
+        return "You win!";
+    }
     console.log(playerScore);
 }
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+//console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-
+   // playRound()
     for (let i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection)
+        console.log (playRound(playerSelection, computerSelection));
+        console.log(i);
+        console.log("computers score :" + compScore);
+        console.log("players score :" + playerScore);
     }
 
 }
