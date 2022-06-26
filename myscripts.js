7d06951d8779803aad0d6bf5808af3d01d6e6922
Andy Wootton <andywootton@gmx.com>
2022-06-26 11:05:57 +0100

    moved computerPlay call into play round to allow computerSelection to change each round, added meaningful messages on return messages

--- myscripts.js
+++ myscripts.js
@@ -1,47 +1,42 @@
-function computerPlay(items) {
+function computerPlay() {
     var items = ['rock', 'paper', 'scissors'];
-    /*var randomItem =  myArray[Math.floor(Math.random()*myArray.length)];
-    return randomItem; */
     return items[Math.floor(Math.random() * items.length)];
-    console.log(computerPlay(items));
+   // console.log(computerPlay(items));
 }
 let playerScore = 0;
 let compScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-    /*let playerSelection = (prompt("Please select rock, paper or scissors "));
-    if(playerSelection == "rock") && (computerSelection == "paper"){ 
-        return "Computer won"
-    }
-    */
-
-
+function playRound(playerSelection) {
+    // added computerPlay to change computerSelection each round
+    const computerSelection = computerPlay();
     if (playerSelection == computerSelection) {
-        return "Tie, no one wins"
+        return "Tie, no one wins";
     } else if ((playerSelection == "paper" && computerSelection === "scissors") ||
         (playerSelection == "scissors" && computerSelection === "rock") ||
         (playerSelection == "rock" && computerSelection === "paper")) {
         compScore = compScore + 1;
-        return "You lose"
+        // added text to show selections into results
+        return "You lose, Your choice of " + playerSelection + " loses to " + computerSelection; 
     } else if ((playerSelection == "rock" && computerSelection === "scissors") || (playerSelection == "paper" && computerSelection === "rock") ||
         (playerSelection == "scissors" && computerSelection === "paper")) {
-        playerScore = playerScore + 1;
-        return "You win!";
+        playerScore = playerScore + 1;#
+        // added text to show selections into results
+        return "You win! Your choice of " + playerSelection + " beats " + computerSelection;
     }
     console.log(playerScore);
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+let  playerSelection = "rock";
+//let computerSelection = computerPlay();
 //console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-   // playRound()
+   // loop for 5 rounds, each round prints player & computer scores and who wins
     for (let i = 0; i < 5; i++) {
-        console.log (playRound(playerSelection, computerSelection));
-        console.log(i);
+        console.log (playRound(playerSelection));
         console.log("computers score :" + compScore);
         console.log("players score :" + playerScore);
+        computerPlay();
     }
-
+    
 }
